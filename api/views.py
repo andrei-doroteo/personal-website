faf86d40a59902950cf2c9df0cc470fa8dbd265d
Adoroteo01 <andreitot13@gmail.com>
2025-04-11 23:08:15 -0700

    added specification for render_markdown endpoint

--- api/views.py
+++ api/views.py
@@ -1,3 +1,27 @@
+import bleach
+import markdown
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
 
 # Create your views here.
+
+
+@require_POST
+def render_markdown(request: HttpRequest) -> HttpResponse:
+    """renders given Markdown code to HTML
+
+    Args:
+        request (HttpRequest): POST request with markdown code
+                               as the body.
+
+    Returns: HttpResponse containing HTML content generated
+             from given markdown code.
+
+    The given markdown input is sanitised using the bleach module to
+    prevent cross site scripting.
+    """
+
+    # TODO
+
+    return HttpResponse("Stub")  # stub
