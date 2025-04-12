a00bee3b1294109d41618c9d27e637fcd10c7106
Adoroteo01 <andreitot13@gmail.com>
2025-04-12 16:21:48 -0700

    added namespace to personal_profile url in project urls

--- personal_website/urls.py
+++ personal_website/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("personal_profile.urls")),
+    path("", include("personal_profile.urls", namespace="personal_profile")),
 ]
